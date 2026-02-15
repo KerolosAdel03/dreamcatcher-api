7c8ae662be1d31ca99d77f3b59a4588267f9dc4b
KerolosAdel03 <126357813+KerolosAdel03@users.noreply.github.com>
2026-02-15 13:37:29 +0200

    Update properties.js

--- api/properties.js
+++ api/properties.js
@@ -1,50 +1,8 @@
 export default async function handler(req, res) {
-  try {
-    // 1. Request Access Token
-    const tokenResponse = await fetch("https://atlas.propertyfinder.com/v1/auth/token", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      },
-      body: JSON.stringify({
-        apiKey: process.env.PF_API_KEY,
-        apiSecret: process.env.PF_API_SECRET
-      })
-    });
-
-    const tokenData = await tokenResponse.json();
-
-    if (!tokenData.accessToken) {
-      return res.status(401).json({
-        step: "token",
-        error: tokenData
-      });
-    }
-
-    // 2. Use Token to Fetch Listings
-    const listingsResponse = await fetch(
-      "https://atlas.propertyfinder.com/v1/listings?page=1&perPage=5",
-      {
-        method: "GET",
-        headers: {
-          "Accept": "application/json",
-          "Authorization": `Bearer ${tokenData.accessToken}`
-        }
-      }
-    );
-
-    const listingsData = await listingsResponse.json();
-
-    return res.status(200).json({
-      step: "listings",
-      data: listingsData
-    });
-
-  } catch (error) {
-    return res.status(500).json({
-      step: "catch",
-      error: error.message
-    });
-  }
+  return res.status(200).json({
+    keyExists: !!process.env.PF_API_KEY,
+    secretExists: !!process.env.PF_API_SECRET,
+    keyLength: process.env.PF_API_KEY ? process.env.PF_API_KEY.length : 0,
+    secretLength: process.env.PF_API_SECRET ? process.env.PF_API_SECRET.length : 0
+  });
 }
